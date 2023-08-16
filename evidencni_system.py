5b2cd76dc515f779220e921c08960a5467c64f7f
domcha1 <106207296+domcha1@users.noreply.github.com>
2023-08-16 13:57:52 +0200

    Update evidencni_system.py

--- evidencni_system.py
+++ evidencni_system.py
@@ -1,19 +1,22 @@
 from pojistenec import Pojistenec
 #Trida evidencniho systemu, jsou v ni funkce, ktere pracuji s instancemi pojistence.
 class Evidencni_System:
-#Konstruktor tridy Evidencni_System
+    #Konstruktor tridy Evidencni_System
     def __init__(self):
         self.pojistenci = []
-#Funkce vytvarejici noveho pojistence do seznamu self.pojistenci
+    
+    #Funkce vytvarejici noveho pojistence do seznamu self.pojistenci
     def vytvor_pojistence(self, jmeno, prijmeni, vek, mobil):
         pojistenec = Pojistenec(jmeno, prijmeni, vek, mobil)
         self.pojistenci.append(pojistenec)
-# Funkce vypisujici vsechny ulozene pojistence ze seznamu self.pojistenci
+    
+    # Funkce vypisujici vsechny ulozene pojistence ze seznamu self.pojistenci
     def vypis_vsechny_pojistence(self):
         for i, pojistenec in enumerate(self.pojistenci, 0):
             print(f"Index: {i}\n {pojistenec}")
             print()
-#Funkce, ktera prohledava seznam self.pojistenci podle zadaneho jmena a  prijmeni (staci pouze jmeno/prijmeni/cast jmena)
+    
+    #Funkce, ktera prohledava seznam self.pojistenci podle zadaneho jmena a  prijmeni (staci pouze jmeno/prijmeni/cast jmena)
     def najdi_pojistence_xy(self, hledane_jmeno, hledane_prijmeni):
         nalezeni_pojistenci = []
         for pojistenec in self.pojistenci:
@@ -25,12 +28,10 @@ class Evidencni_System:
 
         return f"Nikoho jmena {hledane_jmeno} a prijmeni {hledane_prijmeni} jsme nenasli!"
 
-#Funkce, ktera upravy pojistence na konkretnim indexu, rsp. smaze polozku na indexu a vlozi novou
+    #Funkce, ktera upravy pojistence na konkretnim indexu, rsp. smaze polozku na indexu a vlozi novou
     def uprav_pojistence(self, index_pojistence):
         
-
-
-#Funkce, ktera maze pojisteneho podle indexu.
+    #Funkce, ktera maze pojisteneho podle indexu.
     def smaz_pojistence(self, index_pojistence):
        odstraneny_pojistenec = self.pojistenci.pop(index_pojistence)
        return f"Prave jsi na indexu: {index_pojistence} smazal pojistence: \n{odstraneny_pojistenec}"
